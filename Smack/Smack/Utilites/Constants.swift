aaa3ba68e3dbce8eee1088ee056d4e33e008359b
educhin <c.s.arinze@gmail.com>
2017-10-13 13:24:31 -0400

    Lesson 4

---
+++ Smack/Smack/Utilites/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Chinedu Arinze on 10/13/17.
+//  Copyright © 2017 Chinedu Arinze. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
