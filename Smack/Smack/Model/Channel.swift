d285405f866372a0fead2ccfcc412119bec3ef6e
educhin <c.s.arinze@gmail.com>
2017-10-25 00:48:33 -0400

    Lessons 19 & 20. Set up channelVC to have channels added to it. Able to see all channels as a get request was also set up. no bugs present.

---
+++ Smack/Smack/Model/Channel.swift
@@ -0,0 +1,17 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Chinedu Arinze on 10/24/17.
+//  Copyright © 2017 Chinedu Arinze. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+
+}
